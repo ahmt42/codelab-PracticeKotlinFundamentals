178fbd6d873ef764317a69e097c73ea904644e0e
Ahmet Güleç <ahmtgulec@gmail.com>
2023-01-09 22:49:47 +0300

    ""

---
+++ src/main/kotlin/1.MobileNotifications.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val morningNotification = 51
+    val eveningNotification = 135
+
+    printNotificationSummary(morningNotification)
+    printNotificationSummary(eveningNotification)
+}
+
+fun printNotificationSummary(numberOfMessages: Int) {
+    val message = when {
+        numberOfMessages == 0 -> "You have no new messages."
+        numberOfMessages == 1 -> "You have 1 new message."
+        numberOfMessages <= 99 -> "You have $numberOfMessages notifications."
+        else -> "Your phone is blowing up! You have 99+ notifications."
+    }
+    println(message)
+}
