4d244130e3267785f32df963d2da6143b6d6f0a4
Ahmet Güleç <ahmtgulec@gmail.com>
2023-01-15 18:26:08 +0300

    Temperature converter

--- src/main/kotlin/3.TemperatureConverter.kt
+++ src/main/kotlin/3.TemperatureConverter.kt
@@ -1,3 +1,28 @@
+//
+
+/*
+There are three main temperature scales used in the world: Celsius, Fahrenheit, and Kelvin.
+
+In the initial code provided in the following code snippet, write a program that converts a temperature from one scale to another with these formulas:
+
+    Celsius to Fahrenheit: ° F = 9/5 (° C) + 32
+    Kelvin to Celsius: ° C = K - 273.15
+    Fahrenheit to Kelvin: K = 5/9 (° F - 32) + 273.15
+
+Note that the String.format("%.2f", /* measurement */ ) method is used to convert a number into a String type with 2 decimal places.
+ */
+
+/*
+Complete the main() function so that it calls the printFinalTemperature() function and prints the following lines.
+You need to pass arguments for the temperature and conversion formula.
+Hint: you may want to use Double values to avoid Integer truncation during division operations.
+
+27.0 degrees Celsius is 80.60 degrees Fahrenheit.
+350.0 degrees Kelvin is 76.85 degrees Celsius.
+10.0 degrees Fahrenheit is 260.93 degrees Kelvin.
+
+ */
+
 fun main() {
     // Fill in the code.
 }
