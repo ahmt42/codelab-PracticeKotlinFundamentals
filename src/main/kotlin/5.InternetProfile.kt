f42045002d8273c6b6b823e6fc2a895aeae063f9
Ahmet Güleç <ahmtgulec@gmail.com>
2023-01-22 22:20:48 +0300

    Internet profile

--- src/main/kotlin/5.InternetProfile.kt
+++ src/main/kotlin/5.InternetProfile.kt
@@ -1,3 +1,22 @@
+// https://developer.android.com/codelabs/basic-android-kotlin-compose-kotlin-fundamentals-practice-problems#5
+
+/*
+Oftentimes, you're required to complete profiles for online websites that contain mandatory and non-mandatory fields.
+For example, you can add your personal information and link to other people who referred you to sign up for the profile.
+
+In the initial code provided in the following code snippet, write a program which prints out a person's profile details.
+*/
+
+/*
+Name: Amanda
+Age: 33
+Likes to play tennis. Doesn't have a referrer.
+
+Name: Atiqah
+Age: 28
+Likes to climb. Has a referrer named Amanda, who likes to play tennis.
+*/
+
 fun main() {
     val amanda = Person("Amanda", 33, "play tennis", null)
     val atiqah = Person("Atiqah", 28, "climb", amanda)
