c8f064d58e16603b1d7d32b8434c43083c1acfd2
Ahmet Güleç <ahmtgulec@gmail.com>
2023-01-23 21:23:54 +0300

    Foldable phones

--- src/main/kotlin/6.FoldablePhones.kt
+++ src/main/kotlin/6.FoldablePhones.kt
@@ -1,5 +1,18 @@
-class Phone(var isScreenLightOn: Boolean = false){
-    fun switchOn() {
+// https://developer.android.com/codelabs/basic-android-kotlin-compose-kotlin-fundamentals-practice-problems#6
+
+/*
+Typically, a phone screen turns on and off when the power button is pressed.
+In contrast, if a foldable phone is folded, the main inner screen on a foldable phone doesn't turn on when the power button is pressed.
+
+In the initial code provided in the following code snippet, write a FoldablePhone class that inherits from the Phone class. It should contain the following:
+
+A property that indicates whether the phone is folded.
+A different switchOn() function behavior than the Phone class so that it only turns the screen on when the phone isn't folded.
+Methods to change the folding state.
+*/
+
+open class Phone(var isScreenLightOn: Boolean = false){
+    open fun switchOn() {
         isScreenLightOn = true
     }
 
@@ -10,5 +23,21 @@ class Phone(var isScreenLightOn: Boolean = false){
     fun checkPhoneScreenLight() {
         val phoneScreenLight = if(isScreenLightOn) "on" else "off"
         println("The phone screen's light is $phoneScreenLight.")
+    }
+}
+
+class FoldablePhdone(var isFolded: Boolean = false) : Phone() {
+    override fun switchOn() {
+        if (!isFolded) {
+            isScreenLightOn = true
+        }
+    }
+
+    fun fold() {
+        isFolded = true
+    }
+
+    fun unfold() {
+        isFolded = false
     }
 }
